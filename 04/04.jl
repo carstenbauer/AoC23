583f9d4dd3aa79fe4aecbced732a12f4987f6d63
Carsten Bauer <crstnbr@gmail.com>
2023-12-09 15:19:47 +0100

    04

--- 04/04.jl
+++ 04/04.jl
@@ -18,14 +18,18 @@ function parse_card(str)
 	s = split(str, '|')
 	cardnr, winning_nums... = get_integers(s[1])
 	our_nums = get_integers(s[2])
-	return cardnr, winning_nums, our_nums
+	return (nr=cardnr, wnums=winning_nums, onums=our_nums)
+end
+
+# ╔═╡ 94de49b0-2ced-47f0-bdc9-1dc7ac1143a8
+function count_winners(card)
+	nunique = length(union(Set(card.wnums), Set(card.onums)))
+	return length(card.wnums) + length(card.onums) - nunique
 end
 
 # ╔═╡ 42ac9468-878c-42ee-917e-c9d886128d7a
-function calc_card_points(str)
-	cardnr, wnums, onums = parse_card(str)
-	nunique = length(union(Set(wnums), Set(onums)))
-	nwin = length(wnums) + length(onums) - nunique # number of winners 🎉
+function calc_card_points(card)
+	nwin = count_winners(card)
 	return nwin > 0 ? 2^(nwin-1) : 0
 end
 
@@ -37,14 +41,51 @@ end
 					Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 					Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 					Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
-	cards = readlines(IOBuffer(test_input))
+	lines = readlines(IOBuffer(test_input))
+	cards = parse_card.(lines)
 	@test calc_card_points.(cards) == [8, 2, 2, 1, 0, 0]
 end
 
 # ╔═╡ 0003bec4-b507-4ad9-b3d1-cd8cd1fab6ed
 let
 	lines = readlines("04.txt")
-	println("Answer: ", sum(calc_card_points, lines))
+	cards = parse_card.(lines)
+	println("Answer: ", sum(calc_card_points, cards))
+end
+
+# ╔═╡ 4c34c6e6-b6f5-44e8-bf55-bc4a199e91a2
+md"## Part 2"
+
+# ╔═╡ ad6b7939-11fa-4da3-b3e2-eb73f7e19fad
+function count_copied_scratchcards(nr, cards)
+	n = count_winners(cards[nr])
+	n == 0 && return 0
+	return n+sum(i->count_copied_scratchcards(i, cards), nr+1:nr+n)
+end
+
+# ╔═╡ a66c3802-8958-428c-9bca-7ba6bfb21833
+function count_all_scratchcards(cards)
+	return length(cards) + sum(i->count_copied_scratchcards(i,cards), 1:length(cards))
+end
+
+# ╔═╡ 412941b9-0420-4bba-aee1-22e826abbc22
+@testset "part2" begin
+	test_input = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+					Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+					Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+					Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+					Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+					Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
+	lines = readlines(IOBuffer(test_input))
+	cards = parse_card.(lines)
+	@test count_all_scratchcards(cards) == 30
+end
+
+# ╔═╡ 52fe43cc-ed93-443b-8505-23f3ed647fa7
+let
+	lines = readlines("04.txt")
+	cards = parse_card.(lines)
+	println("Answer: ", count_all_scratchcards(cards))
 end
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
@@ -57,7 +98,7 @@ Test = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.10.0-rc1"
+julia_version = "1.9.4"
 manifest_format = "2.0"
 project_hash = "71d91126b5a1fb1020e1098d9d492de2a4438fd2"
 
@@ -76,7 +117,7 @@ deps = ["Base64"]
 uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
 
 [[deps.Random]]
-deps = ["SHA"]
+deps = ["SHA", "Serialization"]
 uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
 
 [[deps.SHA]]
@@ -97,7 +138,13 @@ uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
 # ╠═c242830a-d956-4eb4-8a6b-b5d5749d352f
 # ╠═bac4e18e-1c9d-4252-bd55-52b44bfe8018
 # ╠═7bf411b1-f7e7-4403-a056-0af0375d7a60
+# ╠═94de49b0-2ced-47f0-bdc9-1dc7ac1143a8
 # ╠═42ac9468-878c-42ee-917e-c9d886128d7a
 # ╠═0003bec4-b507-4ad9-b3d1-cd8cd1fab6ed
+# ╟─4c34c6e6-b6f5-44e8-bf55-bc4a199e91a2
+# ╠═ad6b7939-11fa-4da3-b3e2-eb73f7e19fad
+# ╠═a66c3802-8958-428c-9bca-7ba6bfb21833
+# ╠═412941b9-0420-4bba-aee1-22e826abbc22
+# ╠═52fe43cc-ed93-443b-8505-23f3ed647fa7
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
