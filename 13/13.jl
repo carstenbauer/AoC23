767a588cda9ee2232d645fff07468ef5624798de
Carsten Bauer <crstnbr@gmail.com>
2023-12-13 20:04:10 +0100

    13 done

--- 13/13.jl
+++ 13/13.jl
@@ -10,13 +10,18 @@ using Test
 # ╔═╡ 38badd0a-99cd-11ee-3d0e-1b48afb2c443
 md"# Day 13"
 
-# ╔═╡ e6e3b02d-7adf-405c-bd9f-ab9634d331ec
+# ╔═╡ 40d5823a-9a3e-4914-a493-72a024868976
+count_diff(a, b) = count(i -> @inbounds(a[i] != b[i]), eachindex(a))
+
+# ╔═╡ 3c5b7f2c-071c-427b-92e5-ede231d2f38d
 function find_reflections_dim(p, dim)
 	reflections = Int64[]
+	smudge_reflection = 0
 	n = size(p, dim)
 	for c in 1:n-1
 		i = 0
 		isrefl = true
+		ndiff = 0
 		while true
 			l = c-i
 			r = c+1+i
@@ -24,30 +29,35 @@ function find_reflections_dim(p, dim)
 				break
 			end
 			if dim == 1
-				cond = @view(p[l, :]) != @view(p[r,:])
+				ndiff += count_diff(@view(p[l, :]), @view(p[r, :]))
 			elseif dim == 2
-				cond = @view(p[:,l]) != @view(p[:,r])
+				ndiff += count_diff(@view(p[:, l]), @view(p[:, r]))
 			end
-			if cond
+			if ndiff > 0
 				isrefl = false
-				break
 			end
 			i += 1
 		end
 		isrefl && push!(reflections, c)
+		ndiff == 1 && (smudge_reflection = c)
 	end
-	return reflections
+	return (; reflections, smudge_reflection)
 end
 
-# ╔═╡ a66cc1e8-3b90-4e6d-9344-21ca2cad6708
-function find_reflections(p)
-	return (find_reflections_dim(p, 1), find_reflections_dim(p, 2))
+# ╔═╡ 554e0fa0-46a0-40b3-b0fa-2abf76a1cbb8
+function solution_part2(patterns)
+	return sum(patterns) do p
+		r = find_reflections_dim(p, 1).smudge_reflection
+		c = find_reflections_dim(p, 2).smudge_reflection
+		100 * r + c
+	end
 end
 
 # ╔═╡ 2fe5e31a-fb36-4f9c-a61c-c5a7984e4c23
-function solution(patterns)
+function solution_part1(patterns)
 	return sum(patterns) do p
-		r,c = find_reflections(p)
+		r = find_reflections_dim(p, 1).reflections
+		c = find_reflections_dim(p, 2).reflections
 		100 * sum(r) + sum(c)
 	end
 end
@@ -82,19 +92,30 @@ let
 
 	patterns = parse_input(inp)
 	@testset "day13" begin
-		@test find_reflections_dim(patterns[1], 1) == []
-		@test find_reflections_dim(patterns[1], 2) == [5]
-		@test find_reflections_dim(patterns[2], 1) == [4]
-		@test find_reflections_dim(patterns[2], 2) == []
+		p11 = find_reflections_dim(patterns[1], 1)
+		p12 = find_reflections_dim(patterns[1], 2)
+		p21 = find_reflections_dim(patterns[2], 1)
+		p22 = find_reflections_dim(patterns[2], 2)
+		
+		@test p11.reflections == []
+		@test p12.reflections == [5]
+		@test p21.reflections == [4]
+		@test p22.reflections == []
+		@test solution_part1(patterns) == 405
 
-		@test solution(patterns) == 405
+		@test p11.smudge_reflection == 3
+		@test p12.smudge_reflection == 0
+		@test p21.smudge_reflection == 1
+		@test p22.smudge_reflection == 0
+		@test solution_part2(patterns) == 400
 	end
 end
 
 # ╔═╡ b01408fc-851d-4607-b9a4-7dbbd6acf6bb
 let
 	patterns = parse_input("13.txt")
-	println("Answer (Part 1): ", solution(patterns))
+	println("Answer (Part 1): ", solution_part1(patterns))
+	println("Answer (Part 2): ", solution_part2(patterns))
 end
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
@@ -144,10 +165,11 @@ uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
 # ╔═╡ Cell order:
 # ╟─38badd0a-99cd-11ee-3d0e-1b48afb2c443
 # ╠═57b725d0-f696-4f67-822d-e289ba799dfe
-# ╠═791e453b-ac78-4ece-8c8d-6385fdfbbc69
+# ╟─791e453b-ac78-4ece-8c8d-6385fdfbbc69
+# ╠═554e0fa0-46a0-40b3-b0fa-2abf76a1cbb8
 # ╠═2fe5e31a-fb36-4f9c-a61c-c5a7984e4c23
-# ╠═a66cc1e8-3b90-4e6d-9344-21ca2cad6708
-# ╠═e6e3b02d-7adf-405c-bd9f-ab9634d331ec
+# ╠═40d5823a-9a3e-4914-a493-72a024868976
+# ╠═3c5b7f2c-071c-427b-92e5-ede231d2f38d
 # ╠═743e7571-9fa1-44cf-9f19-690964539802
 # ╠═b01408fc-851d-4607-b9a4-7dbbd6acf6bb
 # ╟─00000000-0000-0000-0000-000000000001
