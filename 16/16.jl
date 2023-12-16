496e9946bea774b81688b74450ea0bde6a749d42
Carsten Bauer <crstnbr@gmail.com>
2023-12-16 20:02:39 +0100

    16 1

---
+++ 16/16.jl
@@ -0,0 +1,76 @@
+function parse_input(inp)
+    !isfile(inp) && (inp = IOBuffer(inp))
+    stack(readlines(inp); dims=1)
+end
+
+const RIGHT = CartesianIndex(0, 1)
+const LEFT = CartesianIndex(0, -1)
+const DOWN = CartesianIndex(1, 0)
+const UP = CartesianIndex(-1, 0)
+
+const MOVES = Dict(
+    (RIGHT, '/') => (UP,),
+    (LEFT, '/') => (DOWN,),
+    (UP, '/') => (RIGHT,),
+    (DOWN, '/') => (LEFT,),
+    (RIGHT, '\\') => (DOWN,),
+    (LEFT, '\\') => (UP,),
+    (UP, '\\') => (LEFT,),
+    (DOWN, '\\') => (RIGHT,),
+    (RIGHT, '-') => (RIGHT,),
+    (LEFT, '-') => (LEFT,),
+    (UP, '|') => (UP,),
+    (DOWN, '|') => (DOWN,),
+    (LEFT, '.') => (LEFT,),
+    (RIGHT, '.') => (RIGHT,),
+    (DOWN, '.') => (DOWN,),
+    (UP, '.') => (UP,),
+    (LEFT, '|') => (UP, DOWN),
+    (RIGHT, '|') => (UP, DOWN),
+    (UP, '-') => (LEFT, RIGHT),
+    (DOWN, '-') => (LEFT, RIGHT),
+)
+
+function count_energized(grid)
+    @assert size(grid, 1) == size(grid, 2)
+    n = size(grid, 1)
+    start = (CartesianIndex(1, 1), RIGHT)
+    todo = [start]
+    log = typeof(start)[]
+
+    while !isempty(todo)
+        pos, dir = pop!(todo)
+        if (pos, dir) in log
+            # we have already been here...
+            continue
+        else
+            push!(log, (pos, dir))
+        end
+        c = grid[pos]
+        newdirs = get(MOVES, (dir, c), nothing)
+        isnothing(newdirs) && error("Unknown direction/char combo!")
+        for d in newdirs
+            newpos = pos + d
+            if 1 <= newpos[1] <= n && 1 <= newpos[2] <= n
+                push!(todo, (newpos, d))
+            end
+        end
+    end
+    return length(unique(getindex.(log, 1)))
+end
+
+inp = raw""".|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|...."""
+grid_ex = parse_input(inp)
+cnt = count_energized(grid_ex)
+
+grid = parse_input("16.txt")
+println("Answer (Part 1): ", count_energized(grid))
