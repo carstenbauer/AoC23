962403ff316d65072b3e84b3ac4c5422fe8d31b3
Carsten Bauer <crstnbr@gmail.com>
2023-12-27 11:53:54 +0100

    23

--- 23/23.jl
+++ 23/23.jl
@@ -22,80 +22,88 @@ inp = """#.#####################
 #.....###...###...#...#
 #####################.#"""
 
-using DataStructures
 const CI = CartesianIndex
 const RIGHT = CI(0, 1)
 const DOWN = CI(1, 0)
 const LEFT = -RIGHT
 const UP = -DOWN
 
-struct PartialState
-    pos::CI{2}       # current position
-    hist::Vector{CI{2}} # past positions
-end
-struct State
-    p::PartialState
-    len::Int # current length of path
-end
-State(pos, hist, len) = State(PartialState(pos, hist), len)
-Base.isless(s1::State, s2::State) = s1.len < s2.len
+# grid = stack(readlines(IOBuffer(inp)); dims=1)
+grid = stack(readlines(joinpath(@__DIR__, "23.txt")); dims=1)
 
-function dijkstra(grid)
-    S = CI(1, findfirst(==('.'), first(eachrow(grid))))
-    F = CI(size(grid, 1), findfirst(==('.'), last(eachrow(grid))))
-    init_state = State(S, CI{2}[], 0)
-    seen = Set{PartialState}()
-    Q = BinaryMaxHeap([init_state])
-    # Q = BinaryMinHeap([init_state])
-    results = State[]
-    while !isempty(Q)
-        st = pop!(Q)
-
-        if st.p.pos == F # we're done
-            # return st.len, st
-            push!(results, st)
+function get_relevant_pts(grid)
+    start = CI(1, findfirst(==('.'), grid[1, :]))
+    stop = CI(size(grid, 1), findfirst(==('.'), grid[end, :]))
+    junctions = findall(CartesianIndices(grid)) do p
+        grid[p] == '#' && return false
+        neighs = 0
+        for dir in (UP, RIGHT, DOWN, LEFT)
+            q = p + dir
+            checkbounds(Bool, grid, q) || continue
+            grid[q] != '#' || continue
+            neighs += 1
         end
-        st.p in seen && continue
-        push!(seen, st.p)
+        return neighs >= 3
+    end
+    return vcat(start, stop, junctions)
+end
 
-        for dir in (RIGHT, DOWN, LEFT, UP)
-            pos = st.p.pos + dir
-            checkbounds(Bool, grid, pos) || continue
-            c = grid[pos]
-            c == '#' && continue
-            pos in st.p.hist && continue
-            if dir == RIGHT && c == '<'
-                continue
-            elseif dir == LEFT && c == '>'
-                continue
-            elseif dir == UP && c == 'v'
-                continue
-            elseif dir == DOWN && c == '^'
+function get_reduced_graph(grid, pts; DIRS=Dict(
+    '>' => [RIGHT],
+    '<' => [LEFT],
+    '^' => [UP],
+    'v' => [DOWN],
+    '.' => [UP, RIGHT, LEFT, DOWN],
+))
+    G = Dict(p => Dict{CI{2},Int}() for p in pts)
+    for pt in pts
+        stack = [(pt, 0)]
+        seen = Set((pt,))
+        while !isempty(stack)
+            p, n = pop!(stack)
+
+            if n > 0 && p in pts
+                G[pt][p] = n
                 continue
             end
-            hist = [st.p.hist; st.p.pos]
-            push!(Q, State(pos, hist, st.len + 1))
+
+            c = grid[p]
+            for dir in DIRS[c]
+                q = p + dir
+                checkbounds(Bool, grid, q) || continue
+                grid[q] != '#' || continue
+                !(q in seen) || continue
+                push!(stack, (q, n + 1))
+                push!(seen, q)
+            end
         end
     end
-    return results
+    return G
 end
 
-function parse_input(inp)
-    return stack(readlines(inp); dims=1)
+function dfs(G, pt; seen=Set(), stop)
+    pt == stop && return 0
+    push!(seen, pt)
+    m = -Inf
+    for n in keys(G[pt])
+        n in seen && continue
+        m = max(m, dfs(G, n; seen, stop) + G[pt][n])
+    end
+    delete!(seen, pt)
+    return m
 end
 
-# grid = parse_input(IOBuffer(inp))
-grid = parse_input(joinpath(@__DIR__, "23.txt"))
-st = dijkstra(grid)
+pts = get_relevant_pts(grid)
+start, stop, _ = pts
+G = get_reduced_graph(grid, pts)
+println("Answer (Part 1): ", Int(dfs(G, start; stop)))
 
-# function visualize(grid, st)
-#     G = copy(grid)
-#     G[st.p.hist] .= 'O'
-#     display(G)
-#     println(st.len)
-#     return nothing
-# end
-
-# visualize(grid, st[2])
-
-println("Answer (Part 1): ", maximum(x -> x.len, st))
+DIRS_part2 = Dict(
+    '>' => [UP, RIGHT, LEFT, DOWN],
+    '<' => [UP, RIGHT, LEFT, DOWN],
+    '^' => [UP, RIGHT, LEFT, DOWN],
+    'v' => [UP, RIGHT, LEFT, DOWN],
+    '.' => [UP, RIGHT, LEFT, DOWN],
+)
+G2 = get_reduced_graph(grid, pts; DIRS=DIRS_part2)
+println("Answer (Part 2): ", Int(dfs(G2, start; stop)))
